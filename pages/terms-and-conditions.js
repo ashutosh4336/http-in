004f069a4cc99581338b465f8defe3914d57c210
ashutosh4336 <ashu.juga99@gmail.com>
2022-11-28 23:59:03 +0530

    T&C changes

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@/layouts/Wrapper';
+import styles from '@/styles/T&C.module.scss';
+
+export default function TermsAndCondition() {
+  return (
+    <Layout>
+      <h1>Terms and Conditions</h1>
+    </Layout>
+  );
+}
