c64a7113f2bfb8c51ab4a65dd807c21099e77a22
Ashutosh Panda <ashu.juga99@gmail.com>
2025-03-31 15:32:11 +0530

    Raw JSON Page fixed

--- app/codes/[code]/raw/page.js
+++ app/codes/[code]/raw/page.js
@@ -1,33 +1,49 @@
-import { useRouter } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import { httpStatusCodes } from '@/data/statusCodes';
+import styles from '@/styles/Codes.module.scss';
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
 
-const RawStatusCodePage = () => {
-  const router = useRouter();
-  const { code } = router.query;
-
-  // Find the status code in our data
-  const statusCode = httpStatusCodes.find((sc) => sc.code.toString() === code);
+export async function generateMetadata({ params }) {
+  const code = params.code;
+  const statusCode = httpStatusCodes.find((c) => c.code.toString() === code);
 
   if (!statusCode) {
-    return (
-      <pre style={{ padding: '2rem', fontFamily: 'monospace' }}>
-        {JSON.stringify(
-          {
-            error: 'Not Found',
-            message: `No status code found with number ${code}`,
-          },
-          null,
-          4
-        )}
-      </pre>
-    );
+    return {
+      title: 'Status Code Not Found',
+    };
   }
 
-  return (
-    <pre style={{ padding: '2rem', fontFamily: 'monospace' }}>
-      {JSON.stringify(statusCode, null, 2)}
-    </pre>
-  );
-};
+  return {
+    title: `HTTP ${statusCode.code} - ${statusCode.title}`,
+    description: statusCode.description,
+  };
+}
 
-export default RawStatusCodePage;
+export default function RawStatusCodePage({ params }) {
+  const { code } = params;
+
+  const statusCode = httpStatusCodes.find((c) => c.code.toString() === code);
+
+  if (!statusCode) return notFound();
+
+  delete statusCode.color;
+
+  statusCode.source = new URL('https://thehttp.in').toString();
+  statusCode.author = new URL('https://me.thehttp.in').toString();
+
+  return (
+    <div className={styles.container}>
+      <h1>HTTP Status Code {code}</h1>
+      <pre className={styles.codeBlock}>
+        <code>{JSON.stringify(statusCode, null, 2)}</code>
+      </pre>
+
+      <Link href='/'>
+        <button className={styles.backButton}>
+          <FaArrowLeft /> Back
+        </button>
+      </Link>
+    </div>
+  );
+}
