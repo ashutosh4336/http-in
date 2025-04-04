a575e65a5b1ac73074b30aeccdf069532ce597fe
Ashutosh Panda <ashu.juga99@gmail.com>
2025-04-04 23:43:58 +0530

    Swagger Fix try 1

--- app/api/swagger.json/route.js
+++ app/api/swagger.json/route.js
@@ -1,8 +1,13 @@
 import { generateSwagger } from '@/app/utils/generateSwagger';
+import swaggerSpec from '@/lib/swagger';
 
 export async function GET(request) {
-  const swaggerSpec = await generateSwagger(request);
+  if (process.env.NODE_ENV === 'development') {
+    await generateSwagger(request);
+  }
+
   console.log('swagger.json route called');
+
   return new Response(JSON.stringify(swaggerSpec), {
     headers: { 'Content-Type': 'application/json' },
   });
