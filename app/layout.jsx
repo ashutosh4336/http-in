2f31f8dd100481f26fcb39faa3022e80478ddc56
Ashutosh Panda <ashu.juga99@gmail.com>
2025-04-05 23:27:05 +0530

    Preconnect for gql.hashnode

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,6 +32,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <head>
+        {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' /> */}
+        <link
+          rel='preconnect'
+          href='https://gql.hashnode.com'
+          crossOrigin='anonymous'
+        />
+      </head>
+
       <body className={inter.className}>
         <div className='wrapper'>
           <Navbar />
